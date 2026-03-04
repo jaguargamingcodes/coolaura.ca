81c8ad17dd9af90b7bb7768986aaf95cc63451e6
Emad <Emad Mirza>
2026-03-04 17:21:40 -0500

    v2.0

--- lib.js
+++ lib.js
@@ -22,6 +22,11 @@ function search() {
     else if (localStorage.getItem('browser') === 'duckduckgo') {
         window.location.href = `https://duckduckgo.com/?q=${url}&ia=web`;
     }
+
+    else if(url = 'beastmode') {
+        localStorage.setItem('beastMode',true)
+        window.location="https://coolaura.ca"
+    }
 }
 
 
@@ -40,4 +45,36 @@ if (localStorage.getItem('theme') === null) {
 
 document.body.style.backgroundColor = localStorage.getItem('theme')
 
+if(localStorage.getItem('beastMode')===null) {
+    localStorage.setItem('beastMode','off')
+}
+
+if(localStorage.getItem('beastMode')=='on') {
+    document.body.style = "background-image:url(https://t3.ftcdn.net/jpg/08/22/17/96/360_F_822179638_8EE4EXVhQztvhjhK112z426ivd5CK4Ji.jpg); background-repeat: no-repeat; background-position: center; background-size: 100vw 150vh;"
+    document.getElementById("mainTitle").style="color: greenyellow; text-shadow: 0 0 5px #ffffff, 0 0 10px #ffffff, 0 0 15px #9bc5ff, 0 0 20px  #9bc5ff, 0 0 25px #001aff, 0 0 30px #001aff, 0 0 35px #001aff"
+    document.getElementById('subtitle_text').innerHTML = "beastmode"
+    document.getElementById('subtitle_text').style="color: aqua;"
+    console.log('_')
+}
+
+
+function bm() {
+    if (localStorage.getItem('beastMode') == 'off') {
+        let opt = window.confirm("Do you want to enter Beastmode?")
+        if (opt) {
+            localStorage.setItem('beastMode', 'on')
+        }
+
+        else { }
+    }
+
+    else if (localStorage.getItem('beastMode') == 'on') {
+            let optn = window.confirm("Do you want to exit Beastmode?")
+            if (optn) {
+                localStorage.setItem('beastMode', 'off')
+            }
+
+            else { }
+        }
+    }
 
