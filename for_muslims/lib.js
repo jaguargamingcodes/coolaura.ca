51ed2dfba3c376d64822ff6035ec7d8c499a5675
Emad <Emad Mirza>
2026-03-17 14:34:48 -0400

    test

--- for_muslims/lib.js
+++ for_muslims/lib.js
@@ -31,22 +31,3 @@ let img = window.prompt('Set Wallpaper')
     month: 'long',
     year: 'numeric'
 }
-window.onload = function() {
-const islamicDate = new Intl.DateTimeFormat('en-u-ca-islamic-umalqura-nu-latn', options).format(new Date());
-
-document.getElementById('subtitle_text').innerHTML = islamicDate;
-        const coords = new adhan.Coordinates(43.4516, -80.3123);
-        const date = new Date();
-        const params = adhan.CalculationMethod.NorthAmerica();
-        const prayerTimes = new adhan.PrayerTimes(coords, date, params);
-        document.getElementById('subtitle_text').innerHTML = `
-            <p><b>Fajr:</b> ${prayerTimes.fajr.toLocaleTimeString()}</p>
-            <p><b>Sunrise:</b> ${prayerTimes.sunrise.toLocaleTimeString()}</p>
-            <p><b>Dhuhr:</b> ${prayerTimes.dhuhr.toLocaleTimeString()}</p>
-            <p><b>Asr:</b> ${prayerTimes.asr.toLocaleTimeString()}</p>
-            <p><b>Maghrib:</b> ${prayerTimes.maghrib.toLocaleTimeString()}</p>
-            <p><b>Isha:</b> ${prayerTimes.isha.toLocaleTimeString()}</p>
-        `;
-
-};
-
