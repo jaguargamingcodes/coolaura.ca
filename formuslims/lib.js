2f9278c2b33e6ee2efa17e65de5aff24b6b424dc
Emad <Emad Mirza>
2026-03-17 13:30:28 -0400

    v3

---
+++ formuslims/lib.js
@@ -0,0 +1,28 @@
+if (localStorage.getItem('theme') === null) {
+    localStorage.setItem('theme', 'rgb(19, 19, 19)')
+}
+
+document.body.style.backgroundColor = localStorage.getItem('theme')
+
+
+
+
+function wallpaper() {
+let img = window.prompt('Set Wallpaper')
+    document.getElementById('wallpaper_cont').style.backgroundImage = `url(${img})`
+    localStorage.setItem('wallpapersrc', img)
+}
+
+
+
+    document.getElementById('wallpaper_cont').style.backgroundImage = `url(${localStorage.getItem('wallpapersrc')})`
+
+ function performCustomSearch() {
+    var query = document.getElementById('search').value;
+    if (query) {
+      var element = google.search.cse.element.getElement('searchResults'); // 'searchResults' is the div ID
+      element.execute(query);
+    }
+  }
+
+  
