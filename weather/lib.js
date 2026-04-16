fa7cad03d04f0e49947048e8d1ed35bf17dee061
Emad <Emad Mirza>
2026-04-15 20:30:13 -0400

    test

---
+++ weather/lib.js
@@ -0,0 +1,40 @@
+
+
+
+if(localStorage.getItem('lat') === null || localStorage.getItem('lon') === null) {
+   setLocation();
+}
+
+else {getSetMain}
+
+function getSetMain() {
+  fetch(`https://api.open-meteo.com/v1/forecast?latitude=${localStorage.getItem('lat')}&longitude=${localStorage.getItem('lon')}&current_weather=true`)
+    .then(r => r.json())
+    .then(d => document.getElementById("fc").innerHTML =
+      d.current_weather.temperature + "°C"
+    );
+}
+   
+
+
+
+function setLocation() {
+    document.getElementById('warning').style.display = 'block'
+}
+
+
+function saveLocation() {
+  let lat = document.getElementById('latVal').value;
+  let lon = document.getElementById('lonVal').value;
+   localStorage.setItem('lat', lat)
+   localStorage.setItem('lon', lon)
+   document.getElementById('warning').style.display = 'none'
+   console.log(lat,lon)
+}
+
+
+
+
+
+
+
