329f440011e19a31216f84048404a381f55d9591
Emad <Emad Mirza>
2026-04-16 20:19:13 -0400

    test

--- weather/lib.js
+++ weather/lib.js
@@ -1,40 +1,44 @@
 
 
 
-if(localStorage.getItem('lat') === null || localStorage.getItem('lon') === null) {
-   setLocation();
+if (localStorage.getItem('lat') === null || localStorage.getItem('lon') === null) {
+  setLocation();
 }
 
-if(localStorage.getItem('lat') !== null || localStorage.getItem('lon') !== null) {
-   getSetMain();
+if (localStorage.getItem('lat') !== null || localStorage.getItem('lon') !== null) {
+  getSetMain();
 }
 
 
 
 function getSetMain() {
-  fetch(`https://api.open-meteo.com/v1/forecast?latitude=${localStorage.getItem('lat')}&longitude=${localStorage.getItem('lon')}&current_weather=true`)
+  let mainW =`https://api.open-meteo.com/v1/forecast?latitude=${localStorage.getItem('lat')}&longitude=${localStorage.getItem('lon')}&current=temperature_2m,weather_code,apparent_temperature`
+  fetch(mainW)
     .then(r => r.json())
-    .then(d => document.getElementById("fc").innerHTML =
-      d.current_weather.temperature + "°C"
-    );
-    console.log('test')
+    .then(d => {
+      console.log(d);
+      console.log(d.current.apparent_temperature)
+      document.getElementById("fc").innerHTML = d.current.temperature_2m + "°C";
+      document.getElementById('flc').innerHTML = `feels like ${d.current.apparent_temperature}° C`
+    })
+    .catch(err => console.error("Fetch error:", err));
 }
-   
+
 
 
 
 function setLocation() {
-    document.getElementById('warning').style.display = 'block'
+  document.getElementById('warning').style.display = 'block'
 }
 
 
 function saveLocation() {
   let lat = document.getElementById('latVal').value;
   let lon = document.getElementById('lonVal').value;
-   localStorage.setItem('lat', lat)
-   localStorage.setItem('lon', lon)
-   document.getElementById('warning').style.display = 'none'
-   console.log(lat,lon)
+  localStorage.setItem('lat', lat)
+  localStorage.setItem('lon', lon)
+  document.getElementById('warning').style.display = 'none'
+  console.log(lat, lon)
 }
 
 
