450cc363dae75d932e4c93869801c18a67cc4f81
Emad <Emad Mirza>
2026-08-12 21:39:01 -0400

    testing and editing...

---
+++ game/lib.js
@@ -0,0 +1,43 @@
+document.getElementById('b1').onclick = function () {
+    console.log('Element: b1 clicked')
+}
+
+document.getElementById('b2').onclick = function () {
+    console.log('Element: b2 clicked')
+}
+
+document.getElementById('b3').onclick = function () {
+    console.log('Element: b3 clicked')
+}
+
+document.getElementById('b4').onclick = function () {
+    console.log('Element: b4 clicked')
+}
+
+document.getElementById('b5').onclick = function () {
+    console.log('Element: b5 clicked')
+}
+
+document.getElementById('b6').onclick = function () {
+    console.log('Element: b6 clicked')
+}
+
+document.getElementById('b7').onclick = function () {
+    console.log('Element: b7 clicked')
+}
+
+document.getElementById('b8').onclick = function () {
+    console.log('Element: b8 clicked')
+}
+
+document.getElementById('b9').onclick = function () {
+    console.log('Element: b9 clicked')
+}
+
+
+
+
+
+
+
+
