030f379c2105bad0e1dc831f0ff2b7ee50b1c457
Emad <Emad Mirza>
2026-08-30 22:32:03 -0400

    Version 1.5 (TESTING)

--- lib.js
+++ lib.js
@@ -1,10 +1,3 @@
-import { CreateMLCEngine } from "https://esm.run/@mlc-ai/web-llm";
-const engine = await CreateMLCEngine("Llama-3.2-3B-Instruct-q4f16_1-MLC");
-tInput.onkeydown = async e => {
-  if (e.key !== "Enter") return;
-  response.innerText = "Thinking...";
-  const x = await engine.chat.completions.create({
-    messages: [{ role: "user", content: tInput.value }]
-  });
-  response.innerText = x.choices[0].message.content;
-};
+function showOptions() {
+  document.getElementById('Overlay1').style.display = 'block'
+}
