fbf30f48ff2e4ca25700baa7ca0e89226877c9e7
Emad <Emad Mirza>
2026-08-31 16:41:44 -0400

    Version 1.5 (TESTING)

--- lib.js
+++ lib.js
@@ -1,3 +1,11 @@
 function showOptions() {
-  document.getElementById('Overlay1').style.display = 'block'
+  const overlay = document.getElementById('Overlay1');
+  overlay.style.display = 'block';
+  overlay.style.position = 'fixed'
+  setTimeout(() => {
+    overlay.style.opacity = '1'
+    document.getElementById('SettingsTitle').style.opacity = '1'
+    document.getElementById('sp1').style.opacity = '1'
+    document.getElementById('bar1').style.opacity = '1'
+  }, 10);
 }
