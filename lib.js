b13914a3b6d871792d66ec07a04d50899c4b9592
Emad <Emad Mirza>
2026-09-03 18:35:46 -0400

    Version 1.5 (TESTING)

--- lib.js
+++ lib.js
@@ -9,3 +9,28 @@ function showOptions() {
     document.getElementById('bar1').style.opacity = '1'
   }, 10);
 }
+
+function hideOptions() {
+  const overlay = document.getElementById('Overlay1');
+  overlay.style.display = 'none';
+  overlay.style.position = 'fixed'
+  setTimeout(() => {
+    overlay.style.opacity = '0'
+    document.getElementById('SettingsTitle').style.opacity = '0'
+    document.getElementById('sp1').style.opacity = '0'
+    document.getElementById('bar1').style.opacity = '0'
+  }, 10);
+}
+
+function showCustomizations() {
+  const overlay2 = document.getElementById('Overlay2');
+  overlay2.style.display = 'block';
+  overlay2.style.position = 'fixed'
+  setTimeout(() => {
+    overlay2.style.opacity = '1'
+    document.getElementById('customizationTitle').style.opacity = '1'
+    document.getElementById('2sp1').style.opacity = '1'
+    document.getElementById('bar2').style.opacity = '1'
+  }, 10);
+}
+
