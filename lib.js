3126111b889a913d8e818b7e449e358bb69a9ba9
Emad <Emad Mirza>
2026-09-04 17:29:08 -0400

    Version 1.5 (TESTING)

--- lib.js
+++ lib.js
@@ -1,3 +1,9 @@
+const query = document.getElementById('tInput')
+
+if(localStorage.getItem('se') == null) {
+  localStorage.setItem('se', 'GOOGLE')
+}
+
 if(localStorage.getItem('wallpaper') == null || localStorage.getItem('wallpaper') == "" ) {
 } else {
   document.body.style.backgroundImage = `url(${localStorage.getItem('wallpaper')})`
@@ -17,6 +23,7 @@ function showOptions() {
   }, 10);
 }
 
+
 function hideOptions() {
   const overlay = document.getElementById('Overlay1');
   overlay.style.display = 'none';
@@ -26,6 +33,18 @@ function hideOptions() {
     document.getElementById('SettingsTitle').style.opacity = '0'
     document.getElementById('sp1').style.opacity = '0'
     document.getElementById('bar1').style.opacity = '0'
+  }, 10);
+}
+
+function hideCustomizations() {
+  const overlay2 = document.getElementById('Overlay2');
+  overlay2.style.display = 'none';
+  overlay2.style.position = 'fixed'
+  setTimeout(() => {
+    overlay2.style.opacity = '0'
+    document.getElementById('customizationTitle').style.opacity = '0'
+    document.getElementById('2sp1').style.opacity = '0'
+    document.getElementById('bar2').style.opacity = '0'
   }, 10);
 }
 
@@ -49,3 +68,19 @@ function changeWallpaper(){
   localStorage.setItem('wallpaper', document.getElementById('wpset').value)
   document.body.style.backgroundImage = `url(${localStorage.getItem('wallpaper')})`
 }
+
+function search() {
+  if(localStorage.getItem('se') === 'GOOGLE') {
+    window.location.href = `https://google.com/search?q=${query.value}`;
+  } else if(localStorage.getItem('se') === 'DUCKDUCKGO') {
+    window.location.href = `https://duckduckgo.com/?ai=web&q=${query.value}`;
+  }
+}
+
+query.addEventListener("keydown", function(event) {
+  if(event.key === "Enter") {
+    event.preventDefault();
+    search();
+  }
+})
+
